380dbaa1631edbff48004f64338c601a6bdb74a7
rjtjs <sajetjars@gmail.com>
2023-10-24 22:25:26 -0700

    added ui build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -95,7 +96,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 })
